3eab0e34735238e16e2bc4b0a5bd49d60b5af1c7
Ayush Mukhopadhyay <ayushmukho@gmail.com>
2022-09-12 00:29:33 +0530

    Completed Product requests

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,81 @@
-const Product = require('../models/productModel')
+const Product = require("../models/productModel");
 
+//Create Product - Admin
+exports.createProduct = async (req, res, next) => {
+  const product = await Product.create(req.body);
 
-exports.getAllProduct = (req, res) => {
-  res.status(200).json({ message: "Route is Working" });
+  res.status(201).json({
+    success: true,
+    product,
+  });
 };
+
+//Update Product - Admin
+exports.updateProduct = async (req, res, next) => {
+  let product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return res.status(500).json({
+      success: false,
+      message: "Product not found",
+    });
+  }
+
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+
+  res.status(200).json({
+    success: true,
+    product,
+  });
+};
+
+//Delete Product - Admin
+exports.deleteProduct = async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return res.status(500).json({
+      success: false,
+      message: "Product not found",
+    });
+  }
+
+  await product.remove();
+
+  res.status(200).json({
+    success: true,
+    message: "Product Deleted Successfully",
+  });
+};
+
+//Get all product
+exports.getAllProduct = async (req, res) => {
+  const products = await Product.find();
+
+  res.status(200).json({
+    success: true,
+    products,
+  });
+};
+
+//Get Single Product
+exports.getProductDetails = async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return res.status(500).json({
+      success: false,
+      message: "Product not found",
+    });
+  }
+
+  res.status(200).json({
+    success: true,
+    product,
+  });
+
+}
